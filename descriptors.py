be06179f7ac256aa6af78965cf821900db5aa0e8
Jun Luo <jluo020@uottawa.ca>
2025-06-18 12:49:50 -0400

    code update for 2.0

--- descriptors.py
+++ descriptors.py
@@ -1,214 +1,165 @@
-from itertools import combinations, product
+import torch
+from gemmi import UnitCell
+from torch import Tensor
+from torch_geometric.data import Data
 
-import numpy as np
-
-from elements import en_ghosh
+from elements import element_dict
 
 
-def get_dist_features(cif_struct, symbols, n_atoms):
-    # obtain properties list
-    property_en = np.array([en_ghosh[symbol] for symbol in symbols])
-
-    # determine the number of mirror images required in the supercell
-    max_frac = cif_struct.lattice.get_fractional_coords([8.0, 8.0, 8.0])
-    super_scale = np.ceil(max_frac).astype(int)
-    list_options = list(product(*[range(2 * i + 1) for i in super_scale]))
-    super_options = np.array(list_options, dtype=float) - super_scale
-
-    # create the supercell in both fractional and cartesian coordinates
-    frac_unit = np.array([atom.frac_coords for atom in cif_struct])
-    frac_tiles = np.tile(frac_unit, (len(super_options), 1, 1))
-    super_tiles = np.repeat(super_options, n_atoms, axis=0).reshape(frac_tiles.shape)
-    frac_super = frac_tiles - super_tiles
-    cart_super = cif_struct.lattice.get_cartesian_coords(frac_super)
-    unit_cell_idx = len(super_options) // 2
-    cart_unit = cart_super[unit_cell_idx]
+def featurize(
+    frac_coords: Tensor,
+    cell: UnitCell,
+    atom_symbols: list[str],
+    ap_dict: Tensor,
+):
+    # prepare chemical data
+    atom_nums = torch.tensor([element_dict[k] for k in atom_symbols], dtype=torch.long)
+    ap_tensor = ap_dict["properties"]
+    atom_features = ap_tensor[atom_nums]
+    en_index = ap_dict["en_index"]
+    en_values = atom_features[:, en_index]
+    bonds_mat = ap_dict["bonds"]
 
     # parameters for descriptors
     r_0 = 1.0
     rad_cut = 8.0
     ang_cut = 6.0
-    rdf_bin_size = 0.25
+    rdf_binsize = 0.25
     rdf_alpha = 60.0
-    acsf_rad_bin_size = 0.5
-    acsf_rad_eta = 6.0
-    adf_bin_size_degree = 10
+    racsf_binsize = 0.5
+    racsf_eta = 6.0
+    adf_binsize_deg = 10.0
     adf_beta = 60.0
-    acsf_ang_n_eta = 8
+    aacsf_n_eta = 8
+
+    # create super cells and Cartesian coordinates
+    max_cart = torch.tensor(max((rad_cut, ang_cut)), dtype=torch.float).repeat(3)
+    frac_mat = torch.tensor(cell.frac.mat.tolist(), dtype=torch.float)
+    super_scale = (frac_mat @ max_cart).abs().ceil()
+    list_iters = [torch.arange(i, dtype=torch.float) for i in super_scale * 2 + 1]
+    super_opts = torch.cartesian_prod(*list_iters) - super_scale
+    nso = len(super_opts)
+    frac_tiles = torch.tile(frac_coords, (nso, 1, 1))
+    super_tiles = torch.repeat_interleave(super_opts, len(frac_coords), dim=0)
+    orth_mat = torch.tensor(cell.orth.mat.tolist(), dtype=torch.float)
+    cart_super = (frac_tiles - super_tiles.view(frac_tiles.shape)) @ orth_mat
+    cart_unit = cart_super[nso // 2]
 
     # inits for atomic property weighted radial distribution functions (wRDF)
-    rdf_n_bins = int((rad_cut - r_0) / rdf_bin_size)
-    rdf_edges = np.linspace(r_0 + rdf_bin_size, rad_cut, num=rdf_n_bins)
+    rdf_nbins = int((rad_cut - r_0) / rdf_binsize)
+    rdf_init = r_0 + rdf_binsize
+    rdf_edges = torch.linspace(rdf_init, rad_cut, rdf_nbins, dtype=torch.float)
 
     # inits for radial ACSF
-    acsf_rad_n_bins = int((rad_cut - r_0) / acsf_rad_bin_size)
-    acsf_rad_edges = np.linspace(r_0 + acsf_rad_bin_size, rad_cut, num=acsf_rad_n_bins)
+    racsf_nbins = int((rad_cut - r_0) / racsf_binsize)
+    racsf_init = r_0 + racsf_binsize
+    racsf_edges = torch.linspace(racsf_init, rad_cut, racsf_nbins, dtype=torch.float)
 
     # inits for atomic property weighted angular distribution functions (wADF)
-    adf_bin_size = np.deg2rad(adf_bin_size_degree)
-    adf_n_bins = int(np.pi / adf_bin_size)
-    adf_edges = np.linspace(adf_bin_size, np.pi, num=adf_n_bins)
+    adf_binsize = torch.tensor(adf_binsize_deg, dtype=torch.float).deg2rad()
+    adf_nbins = (torch.pi / adf_binsize).long()
+    adf_edges = torch.linspace(adf_binsize, torch.pi, adf_nbins, dtype=torch.float)
 
     # inits for angular ACSF
-    acsf_ang_edges = np.linspace(r_0, ang_cut, num=acsf_ang_n_eta)
-    acsf_ang_eta = 1 / (2 * (acsf_ang_edges**2))
-    acsf_ang_lambda = [-1, 1]
-    acsf_ang_n_bins = acsf_ang_n_eta * 2
-    acsf_ang_lambda_bins, acsf_ang_eta_bins = np.array(
-        list(product(acsf_ang_lambda, acsf_ang_eta))
-    ).T
+    aacsf_nbins = aacsf_n_eta * 2
+    aacsf_edges = torch.linspace(r_0, ang_cut, aacsf_n_eta, dtype=torch.float)
+    aacsf_eta = 1 / (2 * (aacsf_edges**2))
+    aacsf_lambda = torch.tensor((-1.0, 1.0), dtype=torch.float)
+    aacsf_lambda_bins, aacsf_eta_bins = torch.cartesian_prod(aacsf_lambda, aacsf_eta).T
 
     # generate descriptors for one atom at a time to save memory
     features_list = []
-    for cart_i in cart_unit:
+    bond_pairs_list = []
+    bond_dists_list = []
+    for i, cart_i in enumerate(cart_unit):
         # compute distances between atom i to all other atoms in the supercell
-        dist_to_i = np.linalg.norm(cart_super - cart_i, axis=-1)
+        dist_to_i = torch.linalg.norm(cart_super - cart_i, dim=-1)
+
+        # obtain bonds
+        bonds_super = dist_to_i < bonds_mat[atom_nums, atom_nums[i]]
+        bonds_super &= dist_to_i != 0.0
+        bonds_j = bonds_super.nonzero()[:, 1]
+        bond_pairs_list.append(torch.vstack([torch.full_like(bonds_j, i), bonds_j]))
+        bond_dists_list.append(dist_to_i[bonds_super])
 
         # obtain distances within the cutoffs for radial descriptors
         rad_bool = (dist_to_i >= r_0) & (dist_to_i <= rad_cut)
-        rad_p = property_en[np.nonzero(rad_bool)[1]]
+        rad_p = en_values[torch.nonzero(rad_bool)[:, 1]]
         rad_dist = dist_to_i[rad_bool]
 
         # compute wRDF
-        rdf_diff_bins = (
-            np.repeat(rad_dist, rdf_n_bins).reshape(-1, rdf_n_bins) - rdf_edges
-        )
-        rdf_gauss_bins = np.exp(-rdf_alpha * (rdf_diff_bins**2))
-        rdf = (rdf_gauss_bins.T * rad_p).sum(axis=1)
+        rdf_diff_bins = torch.repeat_interleave(rad_dist, rdf_nbins).view(-1, rdf_nbins)
+        rdf_gauss_bins = (-rdf_alpha * ((rdf_diff_bins - rdf_edges) ** 2)).exp()
+        rdf = (rdf_gauss_bins.T * rad_p).sum(dim=1)
 
         # compute RWAAP
-        wap_rad_btm = rdf_gauss_bins.sum(axis=0)
-        wap_rad = np.divide(
-            rdf, wap_rad_btm, out=np.zeros(rdf_n_bins), where=(wap_rad_btm != 0)
-        )
+        wap_rad_numerator = rdf_gauss_bins.sum(dim=0)
+        wap_rad = torch.zeros(rdf_nbins, dtype=torch.float)
+        rad_mask = wap_rad_numerator != 0
+        wap_rad[rad_mask] = rdf[rad_mask] / wap_rad_numerator[rad_mask]
 
         # compute wRACSF
-        acsf_rad_fcut = (np.cos(rad_dist * np.pi / rad_cut) + 1) * 0.5
-        acsf_rad_diff_bins = (
-            np.repeat(rad_dist, acsf_rad_n_bins).reshape(-1, acsf_rad_n_bins)
-            - acsf_rad_edges
-        )
-        acsf_rad_gauss_bins = np.exp(-acsf_rad_eta * (acsf_rad_diff_bins**2))
-        acsf_rad = (rad_p * acsf_rad_fcut * acsf_rad_gauss_bins.T).sum(axis=1)
+        racsf_fcut = ((rad_dist * torch.pi / rad_cut).cos() + 1) * 0.5
+        racsf_diff_tmp = torch.repeat_interleave(rad_dist, racsf_nbins)
+        racsf_diff_bins = racsf_diff_tmp.view(-1, racsf_nbins) - racsf_edges
+        racsf_gauss_bins = (-racsf_eta * (racsf_diff_bins**2)).exp().T
+        racsf = (rad_p * racsf_fcut * racsf_gauss_bins).sum(dim=1)
+
+        # initialize angular descriptors
+        adf = torch.zeros(adf_nbins, dtype=torch.float)
+        wap_ang = torch.zeros(adf_nbins, dtype=torch.float)
+        aacsf = torch.zeros(aacsf_nbins, dtype=torch.float)
 
         # obtain distances within the cutoffs for angular descriptors
         ang_bool = (dist_to_i >= r_0) & (dist_to_i <= ang_cut)
-        ang_p = property_en[np.nonzero(ang_bool)[1]]
+        ang_p = en_values[torch.nonzero(ang_bool)[:, 1]]
         ang_dist = dist_to_i[ang_bool]
 
-        if len(ang_dist) > 1:
+        if nad := len(ang_dist) > 1:
             # compute angles for all triplets (i,j,k) within the supercell
             ang_sphere_cart = cart_super[ang_bool]
-            atoms_j, atoms_k = np.array(list(combinations(range(len(ang_dist)), 2))).T
+            atoms_j, atoms_k = torch.combinations(torch.arange(nad, dtype=torch.long)).T
             property_jk = ang_p[atoms_j] * ang_p[atoms_k]
-            d_ijk = np.vstack(
+            ang_sphere_subtract = ang_sphere_cart[atoms_j] - ang_sphere_cart[atoms_k]
+            d_ijk = torch.vstack(
                 [
                     ang_dist[atoms_j],
                     ang_dist[atoms_k],
-                    np.linalg.norm(
-                        ang_sphere_cart[atoms_j] - ang_sphere_cart[atoms_k], axis=1
-                    ),
+                    torch.linalg.norm(ang_sphere_subtract, dim=1),
                 ]
             )
             d_ijk_sq = d_ijk**2
-            cos_theta_ijk = (d_ijk_sq[0] + d_ijk_sq[1] - d_ijk_sq[2]) / (
-                2 * d_ijk[0] * d_ijk[1]
-            )
+            ang_numberator = d_ijk_sq[0] + d_ijk_sq[1] - d_ijk_sq[2]
+            ang_denominator = 2 * d_ijk[0] * d_ijk[1]
+            cos_theta_ijk = ang_numberator / ang_denominator
             cos_theta_ijk[cos_theta_ijk < -1.0] = -1.0
             cos_theta_ijk[cos_theta_ijk > 1.0] = 1.0
-            theta_ijk = np.arccos(cos_theta_ijk)
+            theta_ijk = cos_theta_ijk.arccos()
 
             # compute wADF
-            adf_diff_bins = np.tile(theta_ijk, (adf_n_bins, 1)).T - adf_edges
-            adf_gauss_bins = np.exp(-adf_beta * (adf_diff_bins**2))
-            adf = (property_jk * adf_gauss_bins.T).sum(axis=1)
+            adf_diff_bins = torch.tile(theta_ijk, (adf_nbins, 1)).T - adf_edges
+            adf_gauss_bins = (-adf_beta * (adf_diff_bins**2)).exp()
+            adf = (property_jk * adf_gauss_bins.T).sum(dim=1)
 
             # compute AWAAP
-            wap_ang_btm = adf_gauss_bins.sum(axis=0)
-            wap_ang = np.divide(
-                adf, wap_ang_btm, out=np.zeros(adf_n_bins), where=(wap_ang_btm != 0)
-            )
+            wap_ang_numerator = adf_gauss_bins.sum(dim=0)
+            ang_mask = wap_ang_numerator != 0
+            wap_ang[ang_mask] = adf[ang_mask] / wap_ang_numerator[ang_mask]
 
             # compute wAACSF
-            acsf_ang_fcut = ((np.cos(d_ijk * np.pi / ang_cut) + 1) * 0.5).prod(axis=0)
-            acsf_ang_pre = np.tile(cos_theta_ijk, (acsf_ang_n_bins, 1)).T
-            acsf_ang_cos_bins = 1 + acsf_ang_lambda_bins * acsf_ang_pre
-            acsf_ang_dist_bins = np.tile(d_ijk_sq.sum(axis=0), (acsf_ang_n_bins, 1)).T
-            acsf_ang_gauss_bins = np.exp(-acsf_ang_eta_bins * acsf_ang_dist_bins)
-            acsf_ang_bins = (acsf_ang_cos_bins * acsf_ang_gauss_bins).T
-            acsf_ang_bins *= property_jk * acsf_ang_fcut
-            acsf_ang = acsf_ang_bins.sum(axis=1)
-        else:
-            # no angular descriptors computed if there are not enough neighbours
-            adf, wap_ang = np.zeros((2, adf_n_bins))
-            acsf_ang = np.zeros(acsf_ang_n_bins)
+            aacsf_fcut = (((d_ijk * torch.pi / ang_cut).cos() + 1) * 0.5).prod(dim=0)
+            aacsf_pre = torch.tile(cos_theta_ijk, (aacsf_nbins, 1)).T
+            aacsf_cos_bins = 1 + aacsf_lambda_bins * aacsf_pre
+            aacsf_dist_bins = torch.tile(d_ijk_sq.sum(dim=0), (aacsf_nbins, 1)).T
+            aacsf_gauss_bins = (-aacsf_eta_bins * aacsf_dist_bins).exp()
+            aacsf_bins = (aacsf_cos_bins * aacsf_gauss_bins).T
+            aacsf_bins *= property_jk * aacsf_fcut
+            aacsf = aacsf_bins.sum(dim=1)
 
-        features_list.append(
-            np.hstack(
-                [
-                    rdf,
-                    adf,
-                    acsf_rad,
-                    acsf_ang,
-                    wap_rad,
-                    wap_ang,
-                ]
-            ),
-        )
+        features_list.append(torch.hstack([rdf, adf, racsf, aacsf, wap_rad, wap_ang]))
 
-    return np.vstack(features_list)
-
-
-def get_shell_features(bonds_full, property_matrix, n_atoms):
-    # convert adj_mat to adj_list
-    adj_list = [bonds_full[bonds_full[:, 0] == i][:, 1:] for i in range(n_atoms)]
-
-    # init for shell descriptors
-    n_shells = 4
-    n_properties = 8
-    shell_avg = np.zeros((n_atoms, n_properties * n_shells))
-    shell_std = np.zeros((n_atoms, n_properties * n_shells))
-    shell_diff = np.zeros((n_atoms, n_properties * n_shells))
-
-    for source in range(n_atoms):
-        # skip atom if there's no neighbours
-        if len(adj_list[source]) == 0:
-            continue
-
-        # create a tree that represent coordination shells of the source atom
-        tree = [np.array([[source, 0, 0, 0]]), adj_list[source]]
-        for i in range(2, n_shells + 1):
-            if len(tree[-1]) < 1:
-                tree.pop()
-                break
-            nodes = []
-            for bond in tree[-1]:
-                neighbours = adj_list[bond[0]].copy()
-                neighbours[:, 1:] += bond[1:]
-                nodes.append(neighbours)
-            nodes = np.unique(np.vstack(nodes), axis=0)
-            tree.append(nodes)
-            full_levels, idx = np.unique(np.vstack(tree), axis=0, return_index=True)
-            tree[i] = full_levels[idx >= sum([len(tree[lv]) for lv in range(i)])]
-
-        # compute descriptors for each shell
-        self_properties = property_matrix[source]
-        for pos, level in enumerate(tree[1:]):
-            level_properties = property_matrix[level[:, 0]]
-            if len(level) < 1:
-                break
-            elif len(level) == 1:
-                lv_avg = level_properties
-                lv_std = 0.0
-                lv_diff = abs(level_properties - self_properties)
-            else:
-                lv_avg = level_properties.mean(axis=0)
-                lv_std = level_properties.std(axis=0)
-                lv_diff = abs(level_properties - self_properties).mean(axis=0)
-            loc_head = pos * n_properties
-            loc_tail = loc_head + n_properties
-            shell_avg[source, loc_head:loc_tail] = lv_avg
-            shell_std[source, loc_head:loc_tail] = lv_std
-            shell_diff[source, loc_head:loc_tail] = lv_diff
-
-    return np.hstack([shell_avg, shell_std, shell_diff])
+    return Data(
+        x=torch.hstack([atom_features, torch.vstack(features_list)]),
+        edge_index=torch.hstack(bond_pairs_list),
+        edge_attr=torch.hstack(bond_dists_list).view(-1, 1),
+    ).contiguous()
